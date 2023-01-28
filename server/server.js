21bfdae97ce944b29e3a1df513ae32362c2c8d46
NonRoute <chanon.ratn@gmail.com>
2023-01-28 19:30:03 +0700

    Create routes

--- server/server.js
+++ server/server.js
@@ -4,6 +4,9 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 require('dotenv').config()
 
+const indexRoute = require('./routes/index')
+const usersRoute = require('./routes/users')
+
 const app = express()
 
 app.use(express.json())
@@ -12,6 +15,8 @@ app.use(cors())
 app.use(morgan('dev'))
 
 app.get('/', (req, res) => res.send('Hello Schoolar'))
+app.use('/', indexRoute)
+app.use('/users', usersRoute)
 
 const port = process.env.PORT || 8080
 app.listen(port, () => console.log(`start server in port ${port}`))

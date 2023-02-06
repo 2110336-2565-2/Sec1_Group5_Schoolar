61fa3cc5b2c915da3b63fcd679db8ff628f6e07a
Proud <Proud_passamon@hotmail.co.th>
2023-02-06 23:44:29 +0700

    add scholarship file to home page

--- client/src/pages/home-page/index.js
+++ client/src/pages/home-page/index.js
@@ -7,15 +7,10 @@ import PushPinIcon from '@mui/icons-material/PushPin'
 import { grey } from '@mui/material/colors'
 import { useState, useEffect } from 'react'
 import Navbar from '@components/Layout/Navbar'
+import { Scholarship } from './Scholarship'
 
 import Image from 'next/image'
-function Homepage() {
-	const [scholars, setScholars] = useState([])
-
-	useEffect(() => {
-		setScholars([{name: 'S1', tag:['tag1','tag2']},{name: 'S2', tag:['tag3','tag4']}, {name: 'S3', tag:['tag5','tag6']}, {name: 'S4', tag:['tag7','tag8']}, {name: 'S5', tag:['tag9','tag10']}, {name: 'S6', tag:['tag11','tag12']}])
-	}, [])
-	
+function Homepage() {	
 	
 	return (
 		<>
@@ -57,55 +52,11 @@ function Homepage() {
 							</Button>
 						</HStack>
 					</VStack>
-
 					<Typography variant="h5" align="left" color="textPrimary" gutterButtom>
 						The Latest Scholarships
 					</Typography>
 					<Divider orientation="horizontal" flexItem sx={{ borderBottomWidth: 2 }} />
-					<Grid container marginTop={2} gap={8} justifyContent="center">
-						{scholars.map((scholar, idx) => {
-							return (
-								<Paper
-									key= {scholar.name + idx}
-									component="form"
-									sx={{ display: 'flex', width: 300, height: 180, flexDirection: "column" }}
-								>
-									<Grid 
-										container 
-										direction="row" 
-										justifyContent= 'space-between'
-										>
-										<Typography margin={2} marginLeft={2} >
-											{scholar.name}
-										</Typography>
-										<Button
-											variant="text"
-											sx={{ display: 'flex', width: 50, height: 50 }}
-										>
-											<PushPinIcon sx={{ color: grey[900] }} />
-										</Button>
-									</Grid>
-									<Divider orientation="horizontal" borderBottomWidth= {2} />
-									<Grid margin={1}>
-										{scholar.tag.map((tag,idx) => {
-											return(
-												<div key={scholar.name + scholar.tag + idx}>
-													<Box
-														sx={{ display: 'flex', width: 60, height: 25, backgroundColor: '#e6edec', borderRadius: 1, margin: 1, marginLeft: 2}}
-													>
-														<Typography marginLeft={1} >
-															{tag}
-														</Typography>
-													</Box>
-												</div>
-											)
-										})}
-										
-									</Grid>
-								</Paper>
-							)
-						})}
-					</Grid>
+					<Scholarship/>
 				</Container>
 			</div>
 		</>

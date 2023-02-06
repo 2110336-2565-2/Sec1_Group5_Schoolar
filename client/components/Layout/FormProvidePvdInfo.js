9ab2f05d9879238bba407a643ce14c3095e1aaca
mameow <mameow.spmm@gmail.com>
2023-02-06 10:36:36 +0700

    add provide provider information page

---
+++ client/components/Layout/FormProvidePvdInfo.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Button, FormControl, TextField } from '@mui/material'
+import { Stack } from '@mui/system';
+
+const FormProvidePvdInfo = () => {
+    return (
+
+        <FormControl component="form" sx={{ width: '100%' }}>
+
+            <Stack spacing={3} direction="column">
+                <TextField required id="outlined" label="Organization" />
+                <TextField required id="outlined" label="Website" />
+                <TextField required id="outlined" label="Phone Number" />
+                <TextField required id="outlined" label="Email" />
+                <TextField id="outlined" label="Address" />
+
+                <Button variant="contained" type="submit" sx={{ backgroundColor: "#3F51A9" }}>SUBMIT</Button>
+            </Stack>
+
+        </FormControl>
+
+    )
+}
+
+export default FormProvidePvdInfo
--- client/src/pages/register.js
+++ client/src/pages/register.js
@@ -1,5 +1,6 @@
 import FormPrimary from '@components/Layout/FormPrimary'
 import FormProviderStdInfo from '@components/Layout/FormProvideStdInfo'
+import FormProvidePvdInfo from '@components/Layout/FormProvidePvdInfo'
 import FormRegister from '@components/Layout/FormRegister'
 import Error from 'next/error'
 import { useState } from 'react'
@@ -19,7 +20,7 @@ export default function Register() {
 		case 'student':
 			return <FormPrimary header="Student Information" form={<FormProviderStdInfo />} />
 		case 'provider':
-			return <FormPrimary header="Provider Information" form={<FormProviderStdInfo />} />
+			return <FormPrimary header="Provider Information" form={<FormProvidePvdInfo />} />
 		default:
 			return <Error statusCode={404} />
 	}

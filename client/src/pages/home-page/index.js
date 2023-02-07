f1823be6b397a6c545bb2a416752692c2ccc01ec
Nalin Baipluthong <nalinbaipluthong@gmail.com>
2023-02-07 23:21:08 +0700

    devide each component to file

--- client/src/pages/home-page/index.js
+++ client/src/pages/home-page/index.js
@@ -1,60 +1,17 @@
-import { HStack, VStack } from '@components/common'
-import FilterListIcon from '@mui/icons-material/FilterList'
-import SearchIcon from '@mui/icons-material/Search'
-import {
-	Button,
-	Divider,
-	IconButton,
-	InputBase,
-	Paper,
-	Typography,
-} from '@mui/material'
+import { Divider, Typography } from '@mui/material'
 import { Container } from '@mui/system'
-import Image from 'next/image'
 
+import SearchBar from './search-scholarship'
 import { Scholarship } from './scholarship'
 function Homepage() {
-
 	return (
 		<Container maxWidth="lg">
-			<VStack sx={{ p: 3 }} gap={3}>
-				<Image src="/home-page/decor.svg" width="627" height="157"></Image>{' '}
-				<HStack gap={3}>
-					<Paper
-						component="form"
-						sx={{
-							display: 'flex',
-							width: 500,
-							height: 35,
-						}}
-					>
-						<IconButton type="button" sx={{ p: '10px' }}>
-							<FilterListIcon />
-						</IconButton>
-						<InputBase sx={{ ml: 1, flex: 1 }} placeholder="search scholarships" />
-						<IconButton type="button" sx={{ p: '10px' }}>
-							<SearchIcon />
-						</IconButton>
-					</Paper>
-					<Typography variant="h7" align="left" color="textPrimary" gutterButtom>
-						OR
-					</Typography>
-					<Button
-						variant="contained"
-						size="small"
-						sx={{ fontSize: 15, width: 100, height: 35 }}
-						style={{ textTransform: 'none' }}
-					>
-						match
-					</Button>
-				</HStack>
-			</VStack>
-
+			<SearchBar />
 			<Typography variant="h5" align="left" color="textPrimary" gutterButtom>
 				The Latest Scholarships
 			</Typography>
 			<Divider orientation="horizontal" flexItem sx={{ borderBottomWidth: 2 }} />
-			<Scholarship/>
+			<Scholarship />
 		</Container>
 	)
 }

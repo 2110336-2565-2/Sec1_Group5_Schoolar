3ac021f13e6453297840af5337c06406435d1e65
Ryn Parin <ryn.parin@gmail.com>
2023-02-07 18:12:05 +0700

    add protected route

--- server/routes/student.route.js
+++ server/routes/student.route.js
@@ -2,9 +2,10 @@ const express = require('express')
 const router = express.Router()
 const { body } = require('express-validator')
 const { getStudent, updateStudentInfo } = require('../controllers/student.controller')
+const verifyJWT = require('../middleware/verifyJWT')
 
-router.get(getStudent)
+router.get(verifyJWT, getStudent)
 
-router.patch(updateStudentInfo)
+router.patch(verifyJWT, updateStudentInfo)
 
 module.exports = router

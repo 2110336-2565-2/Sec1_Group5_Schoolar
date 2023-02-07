a95160162b8be1949d6434e19dfc48dea21b579c
Ryn Parin <ryn.parin@gmail.com>
2023-02-07 23:05:37 +0700

    fix bug: authen

--- client/src/context/AuthContext.js
+++ client/src/context/AuthContext.js
@@ -1,39 +1,10 @@
 import { createContext, useContext, useEffect, useState } from 'react'
-import jwtDecode from 'jwt-decode'
-import { useRouter } from 'next/router'
-
-import axios from '../pages/api/axios'
 
 const AuthContext = createContext()
 AuthContext.displayName = 'AuthContext'
 
-function AuthContextProvider({ children }) {
-	const [auth, setAuth] = useState(null) //{username, role, accessToken}
-	const router = useRouter()
-
-	console.log(auth)
-
-	useEffect(() => {
-		if (!auth) {
-			axios
-				.get('/auth/refresh-token', {
-					withCredentials: true,
-				})
-				.then(({ data }) => {
-					const { accessToken, role } = data
-					const decodedToken = jwtDecode(accessToken)
-					const user = decodedToken.UserInfo
-					setAuth({ username: user.username, role: role, accessToken: accessToken })
-				})
-				.catch(function (error) {
-					if (error.response.status === 401) {
-						router.push('/login')
-					}
-				})
-		}
-	}, [])
-
-	return <AuthContext.Provider value={{ auth, setAuth }}>{children}</AuthContext.Provider>
+function AuthContextProvider({ value, children }) {
+	return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
 }
 
 function useAuth() {

f32837e70a1b17869ea29928deccd06e3b6290f5
Ryn Parin <ryn.parin@gmail.com>
2023-02-07 11:14:45 +0700

    fixbug: remove require userId

--- server/models/providers.js
+++ server/models/providers.js
@@ -2,47 +2,46 @@ const mongoose = require('mongoose')
 const { Schema } = mongoose
 
 const providerSchema = new Schema({
-    // id: create automatically in mongoDB
-    userID: {
-        required:true,
-        type: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-        // get: write function to get from user ID ,
-        // unique:true,
-        // trim:true
-    },
-    providerName: {
-        required:true,
-        type:String,
-        maxLength:40,
-        lowercase:true,
-        trim:true
-    },
-    address: {
-        required:true,
-        type:String,
-        maxLength:255,
-        lowercase:true,
-        trim:true
-    },
-    website: {
-        required:true,
-        type:String,
-        maxLength:250,
-        lowercase:true,
-        trim:true
-    },
-    creditCardNumber: {
-        required:true,
-        type:Number,
-        maxLength:16,
-        unique:true,
-        trim:true
-    },
-    verifyStatus: {
-        required:true,
-        type:Boolean,
-        trim:true
-    }
+	// id: create automatically in mongoDB
+	userID: {
+		type: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+		// get: write function to get from user ID ,
+		// unique:true,
+		// trim:true
+	},
+	providerName: {
+		required: true,
+		type: String,
+		maxLength: 40,
+		lowercase: true,
+		trim: true,
+	},
+	address: {
+		required: true,
+		type: String,
+		maxLength: 255,
+		lowercase: true,
+		trim: true,
+	},
+	website: {
+		required: true,
+		type: String,
+		maxLength: 250,
+		lowercase: true,
+		trim: true,
+	},
+	creditCardNumber: {
+		required: true,
+		type: Number,
+		maxLength: 16,
+		unique: true,
+		trim: true,
+	},
+	verifyStatus: {
+		required: true,
+		type: Boolean,
+		trim: true,
+	},
 })
 
 module.exports = mongoose.model('Providers', providerSchema)
--- server/models/students.js
+++ server/models/students.js
@@ -2,45 +2,45 @@ const mongoose = require('mongoose')
 const { Schema } = mongoose
 
 const studentSchema = new Schema({
-    // id: create automatically in mongoDB
-    userID: {
-        required:true,
-        type: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-        // get: write function to get from user ID ,
-        // unique:true,
-        // trim:true
-    },
-    firstName: {
-        required:true,     
-        type:String,       
-        // maxLength:40,
-        lowercase:true,
-        trim:true
-    },
-    lastName: {
-        required:true,
-        type:String,
-        maxLength:40,
-        lowercase:true,
-        trim:true
-    },
-    bd: {               //birthdate ,using Date type has some complicated change
-        required:true,
-        type:Date,      //possible to change
-        maxLength:8,
-        // lowercase:false,
-        trim:true
-    },
-    // age: calculate from bd
-    gender: {
-        required:true,
-        type:String,
-        maxLength:10,
-        enum:['male','female'],
-        lowercase:true,
-        trim:true
-    },
-    education: [
+	// id: create automatically in mongoDB
+	userID: {
+		type: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+		// get: write function to get from user ID ,
+		// unique:true,
+		// trim:true
+	},
+	firstName: {
+		required: true,
+		type: String,
+		// maxLength:40,
+		lowercase: true,
+		trim: true,
+	},
+	lastName: {
+		required: true,
+		type: String,
+		maxLength: 40,
+		lowercase: true,
+		trim: true,
+	},
+	bd: {
+		//birthdate ,using Date type has some complicated change
+		required: true,
+		type: Date, //possible to change
+		maxLength: 8,
+		// lowercase:false,
+		trim: true,
+	},
+	// age: calculate from bd
+	gender: {
+		required: true,
+		type: String,
+		maxLength: 10,
+		enum: ['male', 'female'],
+		lowercase: true,
+		trim: true,
+	},
+	education: [
 		{
 			gpax: Number,
 		},
@@ -48,47 +48,50 @@ const studentSchema = new Schema({
 			field: String,
 		},
 	],
-    // below this is the criteria for matching
-    householdIncome: {          // backend tranform number to rank 
-        required:true,
-        type:String,
-        maxLength:15,
-        enum:['high','medium','low'],    // changing later
-        index:true,
-        lowercase:true,
-        trim:true
-    },
-    targetNation: {
-        required:true,
-        type:String,
-        maxLength:40,
-        index:true,
-        lowercase:true,
-        trim:true
-    },
-    typeOfScholarship: {
-        required:true,
-        type:String,
-        maxLength:15,
-        enum:['full','partial','renewable','fellow'],
-        index:true,
-        lowercase:true,
-        trim:true
-    },
-    employment: {           //currently employed or unemployed
-        required:true,
-        type:Boolean,
-        index:true,
-        trim:true
-    },
-    field: {                // field of interest
-        required:true,
-        type:String,
-        maxLength:40,
-        index:true,
-        unique:true,
-        trim:true
-    }
+	// below this is the criteria for matching
+	householdIncome: {
+		// backend tranform number to rank
+		required: true,
+		type: String,
+		maxLength: 15,
+		enum: ['high', 'medium', 'low'], // changing later
+		index: true,
+		lowercase: true,
+		trim: true,
+	},
+	targetNation: {
+		required: true,
+		type: String,
+		maxLength: 40,
+		index: true,
+		lowercase: true,
+		trim: true,
+	},
+	typeOfScholarship: {
+		required: true,
+		type: String,
+		maxLength: 15,
+		enum: ['full', 'partial', 'renewable', 'fellow'],
+		index: true,
+		lowercase: true,
+		trim: true,
+	},
+	employment: {
+		//currently employed or unemployed
+		required: true,
+		type: Boolean,
+		index: true,
+		trim: true,
+	},
+	field: {
+		// field of interest
+		required: true,
+		type: String,
+		maxLength: 40,
+		index: true,
+		unique: true,
+		trim: true,
+	},
 })
 
 module.exports = mongoose.model('Students', studentSchema)

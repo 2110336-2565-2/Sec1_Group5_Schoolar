f69b064d4c93f075b489dec445c34e48e287cee5
Ryn Parin <ryn.parin@gmail.com>
2023-02-07 16:27:20 +0700

    prevent user enter index page

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,3 +1,8 @@
+import { useRouter } from 'next/router'
+
+// TODO: Should we move home-page to this file instead
 export default function Home() {
+	const router = useRouter()
+	typeof window !== 'undefined' && router.push('/home-page')
 	return <div>Hello home page (This is index.js in src)</div>
 }

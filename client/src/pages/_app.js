16919b2df64fdf389134be689ddd5686e1eb8afb
Yaninchaya Unjitwattana <yaninchaya152@hotmail.com>
2023-02-08 13:57:51 +0700

    pull from develop branch and comment auth in app.js

--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -11,34 +11,34 @@ import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
 	const [auth, setAuth] = useState(null) //{username, role, accessToken}
-	const [loading, setLoading] = useState(true)
+	const [loading, setLoading] = useState(false)
 	const [error, setError] = useState(null)
 	const router = useRouter()
 
-	useEffect(() => {
-		setLoading(true)
-		setError(null)
+	// useEffect(() => {
+	// 	setLoading(true)
+	// 	setError(null)
 
-		if (!auth) {
-			axios
-				.get('/auth/refresh-token', {
-					withCredentials: true,
-				})
-				.then(({ data }) => {
-					const { accessToken, role } = data
-					const decodedToken = jwtDecode(accessToken)
-					const user = decodedToken.UserInfo
-					setAuth({ username: user.username, role: role, accessToken: accessToken })
-					setLoading(false)
-				})
-				.catch(function (error) {
-					if (error.response.status === 401) {
-						setError(error)
-						setLoading(false)
-					}
-				})
-		}
-	}, [])
+	// 	if (!auth) {
+	// 		axios
+	// 			.get('/auth/refresh-token', {
+	// 				withCredentials: true,
+	// 			})
+	// 			.then(({ data }) => {
+	// 				const { accessToken, role } = data
+	// 				const decodedToken = jwtDecode(accessToken)
+	// 				const user = decodedToken.UserInfo
+	// 				setAuth({ username: user.username, role: role, accessToken: accessToken })
+	// 				setLoading(false)
+	// 			})
+	// 			.catch(function (error) {
+	// 				if (error.response.status === 401) {
+	// 					setError(error)
+	// 					setLoading(false)
+	// 				}
+	// 			})
+	// 	}
+	// }, [])
 
 	//TODO Fix this UI later
 	if (loading) return <div>Loading...</div>

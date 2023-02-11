1963311e4228fb50c1765cdc4ca81d811942e64b
Yaninchaya Unjitwattana <yaninchaya152@hotmail.com>
2023-02-12 02:20:07 +0700

    fix update std info

--- server/routes/student.route.js
+++ server/routes/student.route.js
@@ -7,5 +7,39 @@ const verifyJWT = require('../middleware/verifyJWT')
 router.get('/', verifyJWT, getStudent)
 
 router.patch('/', verifyJWT, updateStudentInfo)
-
+router.patch(
+	'/student-update',
+	[
+		body('firstName')
+			.isLength({ min: 1 })
+			.withMessage('First Name is Required')
+			.matches(/^[a-zA-Z]*$/)
+			.withMessage('First Name contains invalid characters'),
+		body('lastName')
+			.isLength({ min: 1 })
+			.withMessage('Last Name is Required')
+			.matches(/^[a-zA-Z]*$/)
+			.withMessage('Last Name contains invalid characters'),
+		body('gender').isIn(['Male', 'Female', 'Non-binary']).withMessage('Gender is invalid'),
+		body('birthdate').isISO8601().toDate().withMessage('Birthdate is not in correct format'),
+		body('householdIncome')
+			.isLength({ min: 1 })
+			.withMessage('Household income is required')
+			.matches(/^[0-9]*$]/)
+			.withMessage('Household income must contain only numeric characters'),
+		body('targetNation')
+			.isLength({ min: 1 })
+			.withMessage('Target Nation is required')
+			.isLength({ max: 60 })
+			.withMessage('Target Nation must be at most 60 characters'),
+		body('typeOfScholarship')
+			.isIn(['Full Scholarship', 'Partial Scholarship', 'Reneable Scholarship', 'Followship'])
+			.withMessage('Type of Scholarship is invalid'),
+		body('employment')
+			.isIn(['Employed', 'Unemployed'])
+			.withMessage('Employment status is invalid'),
+		body('field').isLength({ min: 1 }).withMessage('Field of interest is required'),
+	],
+	updateStudentInfo,
+)
 module.exports = router

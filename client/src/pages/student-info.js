ea84ac283c828520fd1660d442a3e09f82ac76c3
Ryn Parin <ryn.parin@gmail.com>
2023-02-14 01:17:57 +0700

    delete student info page

--- client/src/pages/student-info.js
+++
@@ -1,7 +0,0 @@
-import FormPrimary from '@components/Layout/FormPrimary'
-import FormStdInfo from '@components/Layout/FormStdInfo'
-
-export default function UpdateStudentInfo() {
-
-	return <FormPrimary header="Student Information" form={<FormStdInfo />} />
-}

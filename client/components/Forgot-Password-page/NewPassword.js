78f118825a0237d79e74dfe5c7347ff0eb451582
NonRoute <chanon.ratn@gmail.com>
2023-02-24 00:18:27 +0700

    Update NewPassword.js

--- client/components/Forgot-Password-page/NewPassword.js
+++ client/components/Forgot-Password-page/NewPassword.js
@@ -55,117 +55,37 @@ function NewPassword({ router }) {
 		<>
 			{error && <Alert severity="error">Error occur</Alert>}
 			{success && <Alert severity="success">Reset password successfully</Alert>}
-			<Center>
-				<Center
-					sx={{
-						border: '0.2rem solid #2C429B',
-						borderRadius: '1.5rem',
-						width: {
-							xs: '90vw',
-							sm: '500px',
-							xl: '650px',
-						},
-						height: {
-							xs: '60vh',
-							sm: '500px',
-							xl: '650px',
-						},
-						backgroundColor: 'white',
+			<Typography align="center" sx={{ mb: 1.5 }}>
+				Enter your new password
+			</Typography>
+			<FormControl
+				component="form"
+				onSubmit={handleSubmit(onSubmit)}
+				sx={{ display: 'flex', flexDirection: 'column', gap: 2.5, width: '100%' }}
+			>
+				<InputPassword
+					register={{
+						...register('password', getValidation('password')),
 					}}
-				>
-					<Center
-						sx={{
-							border: '0.2rem solid #FDBA21',
-							borderRadius: '1rem',
-							width: {
-								xs: '80vw',
-								sm: '450px',
-								xl: '600px',
+					error={!!errors?.password}
+					helperText={errors?.password ? errors.password.message : null}
+				/>
+				<InputPassword
+					label={'Confirmed Password'}
+					register={{
+						...register('cfpassword', {
+							validate: {
+								match: (value) => value === getValues('password') || getErrMsg('password', 'match'),
 							},
-							height: {
-								xs: '55vh',
-								sm: '450px',
-								xl: '600px',
-							},
-						}}
-					>
-						<FormControl
-							component="form"
-							onSubmit={handleSubmit(onSubmit)}
-							sx={{
-								display: 'flex',
-								gap: [2, 2, 3],
-								width: '80%',
-							}}
-						>
-							<Center>
-								<Box
-									sx={{
-										position: 'relative',
-										width: {
-											xs: '50px',
-											sm: '75px',
-											xl: '100px',
-										},
-										height: {
-											xs: '50px',
-											sm: '75px',
-											xl: '100px',
-										},
-									}}
-								>
-									<Image
-										src={PasswordIcon}
-										alt="Password Icon"
-										layout="fill"
-										objectFit="contain"
-									></Image>
-								</Box>
-							</Center>
-							<Typography
-								fontSize={{
-									xs: '12px',
-									sm: '14px',
-									md: '16px',
-								}}
-								fontWeight={'light'}
-								align="center"
-							>
-								Enter your new password
-							</Typography>
-							<InputPassword
-								register={{
-									...register('password', getValidation('password')),
-								}}
-								error={!!errors?.password}
-								helperText={errors?.password ? errors.password.message : null}
-							/>
-							<InputPassword
-								label={'Confirmed Password'}
-								register={{
-									...register('cfpassword', {
-										validate: {
-											match: (value) =>
-												value === getValues('password') || getErrMsg('password', 'match'),
-										},
-									}),
-								}}
-								error={!!errors?.cfpassword && errors.cfpassword.type === 'match'}
-								helperText={errors?.cfpassword ? errors.cfpassword.message : null}
-							/>
-							<Button
-								fullWidth
-								variant="contained"
-								size="small"
-								style={{ textTransform: 'none' }}
-								type="submit"
-							>
-								Reset Password
-							</Button>
-						</FormControl>
-					</Center>
-				</Center>
-			</Center>
+						}),
+					}}
+					error={!!errors?.cfpassword && errors.cfpassword.type === 'match'}
+					helperText={errors?.cfpassword ? errors.cfpassword.message : null}
+				/>
+				<Button fullWidth variant="contained" size="small" style={{ textTransform: 'none' }} type="submit">
+					Reset Password
+				</Button>
+			</FormControl>
 		</>
 	)
 }

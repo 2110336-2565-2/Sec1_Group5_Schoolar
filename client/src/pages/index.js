30b00670ce381c89e4ecb29e13e03fe21d725159
Nalin Baipluthong <nalinbaipluthong@gmail.com>
2023-02-25 19:05:12 +0700

    edit new home page

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,11 +1,9 @@
 import { useEffect, useState } from 'react'
-import Footer from '@components/Home-page/footer'
 import Scholarship from '@components/Home-page/Scholarship'
 import SearchBar from '@components/Home-page/SearchBar'
-import { Box, Divider, Typography } from '@mui/material'
-import { Container } from '@mui/system'
-
+import { Box, Divider, Typography, FormControl, Container, Paper, Grid, Stack } from '@mui/material'
 import axios from './api/axios'
+import { Center, VStack } from '@components/common'
 
 function Homepage() {
 	const [scholars, setScholars] = useState([])
@@ -26,25 +24,33 @@ function Homepage() {
 	})
 
 	return (
-		<>
-			<Container maxWidth="lg">
+		<Center>
+			<VStack sx={{ p: 5 }}>
 				<SearchBar searchHandler={searchHandler} />
-				<Box sx={{ my: 3 }}>
-					{inputName.length > 0 ? (
-						<Typography variant="h5" align="left" color="textPrimary" gutterBottom>
-							{`Scholarships related to "${inputName}"`}
-						</Typography>
-					) : (
-						<Typography variant="h5" align="left" color="textPrimary" gutterBottom>
-							The Latest Scholarships
-						</Typography>
-					)}
-					<Divider orientation="horizontal" flexItem style={{ borderBottomWidth: 2 }} />
-				</Box>
-				<Scholarship items={filteredScholars} />
-			</Container>
-			<Footer />
-		</>
+				<Paper
+					sx={{
+						minWidth: { xs: 'auto', md: 1150 },
+						borderRadius: 10,
+						padding: 10,
+						backgroundColor: '#F4F6F8',
+					}}
+				>
+					<Box>
+						{inputName.length > 0 ? (
+							<Typography variant="h5" align="left" color="textPrimary" gutterBottom>
+								{`Scholarships related to "${inputName}"`}
+							</Typography>
+						) : (
+							<Typography variant="h5" align="left" color="textPrimary" gutterBottom>
+								The Latest Scholarships
+							</Typography>
+						)}
+						<Divider orientation="horizontal" flexItem style={{ borderBottomWidth: 2 }} />
+					</Box>
+					<Scholarship items={filteredScholars} />
+				</Paper>
+			</VStack>
+		</Center>
 	)
 }
 

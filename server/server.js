e3aa4f9c00643d91cb06941787a1a89006d1e972
Ryn Parin <ryn.parin@gmail.com>
2023-02-27 01:41:37 +0700

    structure files

--- server/server.js
+++ server/server.js
@@ -1,29 +1,16 @@
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
-const mongoose = require('mongoose')
 const cookieParser = require('cookie-parser')
 const swaggerUi = require('swagger-ui-express')
 const swaggerDocument = require('./swagger.json')
+const routes = require('./routes')
+const { connect } = require('./configs/db')
 require('dotenv').config()
-
-const indexRoute = require('./routes/index')
-const authRoute = require('./routes/auth.route')
-const studentRoute = require('./routes/student.route')
-const resetPasswordRoute = require('./routes/resetPassword.route')
-const scholarshipRoute = require('./routes/scholarship.route')
-const providerRoute = require('./routes/provider.route')
-const userRoute = require('./routes/user.route')
 
 const app = express()
 
-mongoose.set('strictQuery', false)
-mongoose
-	.connect(process.env.DATABASE, { autoIndex: true })
-	.then((result) => {
-		console.log('mongoose connected!')
-	})
-	.catch((err) => console.log(err))
+connect()
 
 app.use(cookieParser())
 app.use(express.json())
@@ -32,17 +19,10 @@ app.use(cors({ origin: true, credentials: true }))
 app.use(morgan('dev'))
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
-app.use('/', indexRoute)
-app.use('/auth', authRoute)
-app.use('/student', studentRoute)
-app.use('/resetPassword', resetPasswordRoute)
-app.use('/scholarship', scholarshipRoute)
-app.use('/provider', providerRoute)
-app.use('/user', userRoute)
+app.use('/', routes)
 
 const port = process.env.PORT || 8080
-app.listen(port, () =>
-	console.log(
-		`start server in port ${port}\nto update API doc: "npm run swagger-autogen"\nAPI documentation: http://localhost:${port}/api-docs`,
-	),
-)
+app.listen(port, () => {
+	console.log(`Server started on port ${port}`)
+	console.log(`API documentation: http://localhost:${port}/api-docs`)
+})

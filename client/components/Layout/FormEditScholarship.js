ff8176d91aee2e970161b4a209e13845d3e89f01
Proud <Proud_passamon@hotmail.co.th>
2023-02-28 00:04:58 +0700

    edit scholarship with api

--- client/components/Layout/FormEditScholarship.js
+++ client/components/Layout/FormEditScholarship.js
@@ -1,14 +1,15 @@
-import React , {useEffect}from 'react'
+import React, { useEffect } from 'react'
 import { useForm } from 'react-hook-form'
 
-import { Button, FormControl, Grid, Stack, MenuItem, Select, InputLabel} from '@mui/material'
+import { Button, FormControl, Grid, Stack } from '@mui/material'
 import { DatePickerComponent, SelectComponent, TextFieldComponent } from '@utils/formComponentUtils'
+import { useRouter } from 'next/router'
 
 import { useAuth } from '@/context/AuthContext'
 import useAxiosPrivate from '@/hooks/useAxiosPrivate'
 
-const FormEditScholarship = () => {
-
+const FormEditScholarship = ({ id }) => {
+	const router = useRouter()
 	const {
 		register,
 		handleSubmit,
@@ -26,12 +27,36 @@ const FormEditScholarship = () => {
 	const axiosPrivate = useAxiosPrivate()
 
 	useEffect(() => {
-
+		axiosPrivate.get(`/scholarship/${id}`).then((res) => {
+			const data = res.data.data
+			reset({
+				provider: auth.username,
+				scholarshipName: data.scholarshipName,
+				gpax: data.gpax,
+				degree: data.degree,
+				targetNation: data.targetNation,
+				program: data.program,
+				amount: data.amount,
+				quota: data.quota,
+				fieldOfInterest: data.fieldOfInterest,
+				typeOfScholarship: data.typeOfScholarship,
+				detail: data.detail,
+				applicationDeadline: data.applicationDeadline,
+			})
+		})
 	}, [])
 
 	const onSubmit = (e) => {
-		console.log(e)
-		alert(JSON.stringify(e))
+		axiosPrivate
+			.put(`/scholarship/${id}`, {
+				amount: e.amount,
+				quota: e.quota,
+				detail: e.detail,
+			})
+			.then(() => {
+				alert('Update successfully')
+				router.push('/')
+			})
 	}
 
 	const formProps = { register, errors, getValues, setValue, control, watch }
@@ -39,20 +64,20 @@ const FormEditScholarship = () => {
 		<Stack>
 			<FormControl component="form" onSubmit={handleSubmit(onSubmit)} sx={{ width: '100%' }}>
 				<Stack spacing={3} direction="column">
-					<TextFieldComponent name="scholarshipName" {...formProps} />
-					<TextFieldComponent name="provider" {...formProps} />
+					<TextFieldComponent name="scholarshipName" disabled={true} shrink={true} {...formProps} />
+					<TextFieldComponent name="provider" disabled={true} shrink={true} {...formProps} />
 					<h3>Requirement</h3>
-					<TextFieldComponent name="gpax" {...formProps} />
-					<SelectComponent name="degree" {...formProps}/>
-					<TextFieldComponent name="targetNation" {...formProps} />
-					<SelectComponent name="program" {...formProps} />
+					<TextFieldComponent name="gpax" disabled={true} shrink={true} {...formProps} />
+					<SelectComponent name="degree" disabled={true} shrink={true} {...formProps} />
+					<TextFieldComponent name="targetNation" disabled={true} shrink={true} {...formProps} />
+					<SelectComponent name="program" disabled={true} shrink={true} {...formProps} />
 					<h3>Detail of scholarship</h3>
-					<TextFieldComponent name="amount" label="Amount (Baht)" {...formProps} />
-					<TextFieldComponent name="quota" label="Scholarship Quota" {...formProps} />
-					<TextFieldComponent name="fieldOfInterest" {...formProps} />
-					<SelectComponent name="typeOfScholarship" {...formProps} />
-					<TextFieldComponent name="detail" label="More Details" {...formProps} />
-					<DatePickerComponent name="applicationDeadline" {...formProps} />
+					<TextFieldComponent name="amount" label="Amount (Baht)" shrink={true} {...formProps} />
+					<TextFieldComponent name="quota" label="Scholarship Quota" shrink={true} {...formProps} />
+					<TextFieldComponent name="fieldOfInterest" disabled={true} shrink={true} {...formProps} />
+					<SelectComponent name="typeOfScholarship" disabled={true} shrink={true} {...formProps} />
+					<TextFieldComponent name="detail" label="More Details" shrink={true} {...formProps} />
+					<DatePickerComponent name="applicationDeadline" disabled={true} shrink={true} {...formProps} />
 				</Stack>
 				<Grid
 					container
@@ -62,7 +87,12 @@ const FormEditScholarship = () => {
 					sx={{ padding: '20px 0px 20px 20px' }}
 				>
 					<Grid item>
-						<Button variant="contained" type="submit">
+						<Button
+							variant="contained"
+							onClick={() => {
+								router.push('/')
+							}}
+						>
 							Cancel
 						</Button>
 					</Grid>

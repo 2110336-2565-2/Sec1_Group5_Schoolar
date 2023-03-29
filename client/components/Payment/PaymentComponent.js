c8362cbc8b9acf26e44074968470166a198cae1e
Nichapa Nittayasittikun <Nichapanit555@gmail.com>
2023-03-29 20:02:45 +0700

    create payment component success

--- client/components/Payment/PaymentComponent.js
+++ client/components/Payment/PaymentComponent.js
@@ -1,14 +1,43 @@
 import { React } from 'react'
-import { grey,blue } from '@mui/material/colors'
+import { grey,blue, red } from '@mui/material/colors'
+import { Box, Button, Stack, Divider, Typography, Grid} from '@mui/material'
+import PushPinIcon from '@mui/icons-material/PushPin'
+import { CenterFocusStrong } from '@mui/icons-material'
 
-function paymentComponent() {
+function PaymentComponent() {
+    var scholarName = "Scholarship1"
 	return (
-        <div>
-            <h1>Start</h1>
-            <p>testttttttttttttt</p>
-        </div>
-        
+        <Box sx={{ 
+            width: '100%', 
+            height: '25%', 
+            margin: 2, 
+            backgroundColor: '#e6edec',
+            borderRadius: 5}}>
+			<Grid container direction="row" justifyContent="space-between">
+				<Grid sx={{
+                    width: '60%'}}>
+                    <Typography variant='h6' padding={2.5} marginLeft={2}>
+					    {scholarName}
+				    </Typography>
+                </Grid>
+                <Grid container direction = "column" justifyContent="space-between" sx={{
+                    width: '30%',
+                    padding: 2.5}}>
+                    <Typography variant='body1' align='center'>
+					    Next payment: 7 days
+				    </Typography>
+                    <Grid sx={{pl:'25%',pr:'25%',pt:'2%'}}>
+                        <Button variant="contained" sx={{ borderRadius: 5, width: '100%'}}>
+					        Subscribe
+				        </Button>
+                    </Grid>
+                    <Typography variant='body1' align='center'>
+					    99$ / month
+				    </Typography>
+                </Grid>
+			</Grid>							
+		</Box>
 	)
 }
 
-export default paymentComponent
+export default PaymentComponent
--- client/src/pages/payment/paymentList.js
+++ client/src/pages/payment/paymentList.js
@@ -1,8 +1,8 @@
 import { React } from 'react'
-import PaymentComponent from '@components/Payment/paymentComponent'
+import PaymentComponent from '@components/Payment/PaymentComponent'
 import FormSecondary from '@components/Layout/FormSecondary'
 import { Center } from '@components/common'
 
 export default function paymentList() {
-    return <FormSecondary header="Payment" form={<PaymentComponent />} />
+    return <PaymentComponent/>
 }

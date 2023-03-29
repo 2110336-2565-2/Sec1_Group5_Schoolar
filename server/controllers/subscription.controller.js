3159e7d7a6723d9e6b55ebb546646f47486b6a2e
watcharavit <watcha.ea@hotmail.com>
2023-03-29 20:45:19 +0700

    get payment history both success and unsuccess

--- server/controllers/subscription.controller.js
+++ server/controllers/subscription.controller.js
@@ -52,44 +52,25 @@ exports.getNextPaymentDate = async (req, res) => {
 }
 
 /*
- * @desc     Get All Subscription Payment History
- * @route    GET /subscription/payment-history
- * @access   Private
- */
-exports.getAllSubscriptionPaymentHistory = async (req, res) => {
-	try {
-		const paymentIntents = await stripe.paymentIntents.list();
-		const paymentHistory = paymentIntents.data.map(payment => {
-			return {
-				id: payment.id,
-				created: new Date(payment.created * 1000),
-				amount: payment.amount,
-				currency: payment.currency
-			};
-		});
-		res.status(200).json(paymentHistory);
-	} catch (error) {
-		res.status(500).json({ error: error.message });
-	}
-};
-
-/*
  * @desc     Get Subscription Payment History by subscription id
- * @route    GET /payment-history/:id
+ * @route    GET /paymentHistory/:subscriptionId
  * @access   Private
  */
 exports.getSubscriptionPaymentHistory = async (req, res) => {
+	const subscriptionId = req.params.subscriptionId
 	try {
-		const subscriptionId = req.params.id;
-		const invoices = await stripe.invoices.list({ subscription: subscriptionId });
-		const paymentHistory = invoices.data.map(invoice => ({
-			id: invoice.id,
-			date: new Date(invoice.created * 1000),
-			amount: invoice.amount_paid,
-			currency: invoice.currency
-		}));
-		res.status(200).json(paymentHistory);
+		// Get the invoice list for the given subscription
+		const invoices = await stripe.invoices.list({
+			subscription: subscriptionId,
+			limit: 24,
+		})
+		const history = { paid: [], uncollectible: [] }
+		// Iterate through the invoices and print the payment details
+		for (let invoice of invoices.data) {
+			history[invoice.status].push(new Date(invoice.created * 1000).toLocaleString())
+		}
+		return res.status(200).json({ history: history })
 	} catch (error) {
-		res.status(500).json({ error: error.message });
+		console.error(`Error fetching payment history: ${error.message}`)
 	}
-};
+}
--- server/routes/subscription.route.js
+++ server/routes/subscription.route.js
@@ -5,15 +5,13 @@ const {
 	getSubscription,
 	getSubscriptions,
 	getNextPaymentDate,
-	getAllSubscriptionPaymentHistory,
+	// getAllSubscriptionPaymentHistory,
 	getSubscriptionPaymentHistory,
 } = require('../controllers/subscription.controller')
 const verifyJWT = require('../middleware/verifyJWT')
 
-router.get('/payment-history', verifyJWT, getAllSubscriptionPaymentHistory)
 router.get('/', verifyJWT, getSubscriptions)
 router.get('/:id', verifyJWT, getSubscription)
 router.get('/next-payment-date/:id', verifyJWT, getNextPaymentDate)
-router.get('/payment-history/:id', verifyJWT, getSubscriptionPaymentHistory)
-
+router.get('/paymentHistory/:subscriptionId', verifyJWT, getSubscriptionPaymentHistory)
 module.exports = router

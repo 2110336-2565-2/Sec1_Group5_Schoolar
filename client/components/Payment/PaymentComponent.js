1648f50b6e91ac42a50b9e2fe83e4378e9508d7c
Ryn Parin <ryn.parin@gmail.com>
2023-03-30 02:48:56 +0700

    integrate with backend api

--- client/components/Payment/PaymentComponent.js
+++ client/components/Payment/PaymentComponent.js
@@ -1,41 +1,139 @@
+import { useEffect, useState } from 'react'
 import { React } from 'react'
-import { grey,blue, red } from '@mui/material/colors'
-import { Box, Button, Stack, Divider, Typography, Grid} from '@mui/material'
-import PushPinIcon from '@mui/icons-material/PushPin'
-import { CenterFocusStrong } from '@mui/icons-material'
 
-function PaymentComponent() {
-    var scholarName = "Scholarship1"
+import { Box, Button, Divider, Grid, Stack, Typography } from '@mui/material'
+import { useRouter } from 'next/router'
+
+import useAxiosPrivate from '@/hooks/useAxiosPrivate'
+
+function PaymentComponent({ scholarship }) {
+	const axiosPrivate = useAxiosPrivate()
+	const router = useRouter()
+	const handleSubscribe = async () => {
+		try {
+			const res = await axiosPrivate.post(`/subscription/checkout/${scholarship._id}`)
+			router.push(res.data.url)
+		} catch (err) {
+			console.log(err)
+		}
+	}
+
+	//! No endpoint yet
+	const handleUnSubscribe = async () => {
+		try {
+			const res = await axiosPrivate.delete(`/subscription/${scholarship.subscription}`)
+			return res.data
+		} catch (err) {
+			console.log(err)
+		}
+	}
+
+	const getNextPaymentDate = async () => {
+		try {
+			const res = await axiosPrivate.get(`/subscription/next-payment-date/${scholarship.subscription}`)
+			return res.data
+		} catch (err) {
+			console.log(err)
+		}
+	}
+
+	const calculateNextPaymentDate = async () => {
+		try {
+			const nextDate = await getNextPaymentDate()
+			const year = parseInt(nextDate.slice(0, 4))
+			const month = parseInt(nextDate.slice(5, 7)) - 1
+			const day = parseInt(nextDate.slice(8, 10))
+			const hour = parseInt(nextDate.slice(11, 13))
+			const minute = parseInt(nextDate.slice(14, 16))
+			const second = parseInt(nextDate.slice(17, 19))
+			const millisecond = parseInt(nextDate.slice(20, 23))
+
+			const date = new Date(Date.UTC(year, month, day, hour, minute, second, millisecond))
+			const timeDiff = date.getTime() - Date.now()
+			const daysTillNextPayment = Math.ceil(timeDiff / (1000 * 60 * 60 * 24))
+			return daysTillNextPayment
+		} catch (err) {
+			console.log(err)
+		}
+	}
+
+	const [nextPaymentDate, setNextPaymentDate] = useState(0)
+	const [isSubscribed, setIsSubscribed] = useState(false)
+
+	useEffect(() => {
+		if (scholarship.subscription !== undefined) {
+			setIsSubscribed(true)
+			const calculateAndSetNextPaymentDate = async () => {
+				const result = await calculateNextPaymentDate()
+				setNextPaymentDate(result)
+			}
+			calculateAndSetNextPaymentDate()
+		}
+	}, [])
+
+	// TODO: FIX UI CRASH
 	return (
-        <Box sx={{ 
-            width: '100%', 
-            height: '25%', 
-            margin: 2, 
-            backgroundColor: '#e6edec',
-            borderRadius: 5}}>
+		<Box
+			sx={{
+				width: '100%',
+				height: '25%',
+				margin: 2,
+				backgroundColor: '#e6edec',
+				borderRadius: 5,
+			}}
+		>
 			<Grid container direction="row" justifyContent="space-between">
-				<Grid sx={{
-                    width: '60%'}}>
-                    <Typography variant='h6' padding={2.5} marginLeft={2}>
-					    {scholarName}
-				    </Typography>
-                </Grid>
-                <Grid container direction = "column" justifyContent="space-between" sx={{
-                    width: '30%',
-                    padding: 2.5}}>
-                    <Typography variant='body1' align='center'>
-					    Next payment: 7 days
-				    </Typography>
-                    <Grid sx={{pl:'25%',pr:'25%',pt:'2%'}}>
-                        <Button variant="contained" sx={{ borderRadius: 5, width: '100%'}}>
-					        Subscribe
-				        </Button>
-                    </Grid>
-                    <Typography variant='body1' align='center'>
-					    99$ / month
-				    </Typography>
-                </Grid>
-			</Grid>							
+				<Grid
+					sx={{
+						width: '60%',
+					}}
+				>
+					<Typography variant="h6" padding={2.5} marginLeft={2}>
+						{scholarship.scholarshipName}
+					</Typography>
+				</Grid>
+				<Grid
+					container
+					direction="column"
+					justifyContent="space-between"
+					sx={{
+						width: '30%',
+						padding: 2.5,
+					}}
+				>
+					{/* //? จัดการกับเดดไลน์การ subscribe ยังไงเพราะไม่มีการเก็บวันที่สร้าง scholarship ใน db */}
+					{isSubscribed && (
+						<Typography variant="body1" align="center">
+							Next payment: <span style={{ color: '#FF0000' }}>{nextPaymentDate} days</span>
+						</Typography>
+					)}
+
+					<Grid sx={{ pl: '25%', pr: '25%', pt: '2%' }}>
+						{isSubscribed ? (
+							<Button
+								variant="contained"
+								sx={{ borderRadius: 5, width: '100%', backgroundColor: '#C1C1C1' }}
+								onClick={handleUnSubscribe}
+							>
+								Subscribed
+							</Button>
+						) : (
+							<Button
+								variant="contained"
+								sx={{ borderRadius: 5, width: '100%' }}
+								onClick={handleSubscribe}
+							>
+								Subscribe
+							</Button>
+						)}
+					</Grid>
+					{!isSubscribed && (
+						<Typography variant="body1" align="center" color="#9B9B9B">
+							99฿ / month
+						</Typography>
+					)}
+				</Grid>
+			</Grid>
 		</Box>
 	)
 }
--- client/src/pages/payment/paymentList.js
+++ client/src/pages/payment/paymentList.js
@@ -1,8 +1,63 @@
 import { React } from 'react'
+import { useEffect, useState } from 'react'
+
 import PaymentComponent from '@components/Payment/PaymentComponent'
-import FormSecondary from '@components/Layout/FormSecondary'
-import { Center } from '@components/common'
+import { Center, VStack } from '@components/common'
+
+import useAxiosPrivate from '@/hooks/useAxiosPrivate'
 
 export default function paymentList() {
-    return <PaymentComponent/>
+	const [scholars, setScholars] = useState([])
+	const axiosPrivate = useAxiosPrivate()
+
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const res = await axiosPrivate.get('/scholarship')
+				setScholars(res.data.data)
+				console.log(res.data.data)
+			} catch (error) {
+				console.log(error)
+			}
+		}
+		fetchData()
+	}, [])
+
+	const mockData = {
+		amount: 15,
+		applicationDeadline: '2022-08-01T00:00:00.000Z',
+		degree: 'bachelor',
+		detail: 'buggg',
+		fieldOfInterest: 'Computer',
+		gpax: 4,
+		program: 'Faculty of Arts',
+		provider: '63fb928bad8d592179496311',
+		quota: 20,
+		scholarshipName: 'xyz',
+		subscription: 'sub_1Mqc7vCn18gHMRofq7n4LovF',
+		targetNation: 'tha',
+		typeOfScholarship: 'full',
+		_id: '63fc86349ab2c8cd513613e6',
+	}
+	const mockData2 = {
+		amount: 15,
+		applicationDeadline: '2022-08-01T00:00:00.000Z',
+		degree: 'bachelor',
+		detail: 'buggg',
+		fieldOfInterest: 'Computer',
+		gpax: 4,
+		program: 'Faculty of Arts',
+		provider: '63fb928bad8d592179496311',
+		quota: 20,
+		scholarshipName: 'xyz',
+		targetNation: 'tha',
+		typeOfScholarship: 'full',
+		_id: '63fc86349ab2c8cd513613e6',
+	}
+	return (
+		<VStack>
+			<PaymentComponent scholarship={mockData} />
+			<PaymentComponent scholarship={mockData2} />
+		</VStack>
+	)
 }

84cd74a938c8c1cebec5ef57331c78b08847394e
NonRoute <chanon.ratn@gmail.com>
2023-03-30 01:51:45 +0700

    Make scholarship sort by pin

--- client/components/Home-page/Scholarship.js
+++ client/components/Home-page/Scholarship.js
@@ -1,25 +1,30 @@
+import { useState } from 'react'
+import { useEffect } from 'react'
+
 import PushPinIcon from '@mui/icons-material/PushPin'
 import { Box, Button, Divider, Grid, Paper, Typography } from '@mui/material'
-import { useState } from 'react'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
+
 import { useAuth } from '@/context/AuthContext'
-import PinScholar from './PinScholar'
 import { axiosPrivate } from '@/pages/api/axios'
 
+import PinScholar from './PinScholar'
 
 function Scholarship(props) {
 	const { auth } = useAuth()
 	const router = useRouter()
-	//total scholarship and pin sholarship
-	const std = axiosPrivate.get(`/student/${auth.username}`)
-	var pinStd = std.pinScholarships
-	var scholarship = props.items.scholar
-	console.log("pinStd:",pinStd)
-	console.log("scholarship",scholarship)
-	//sort scholarship
-	//scholarship = scholarship.sort();
-	//pinStd = pinStd.sort();
+	const [pinScholarships, setPinScholarships] = useState([])
+
+	const fetchData = async () => {
+		const student = await axiosPrivate.get(`/student/${auth.username}`)
+		setPinScholarships(student.data.student.pinScholarships)
+	}
+
+	useEffect(() => {
+		fetchData()
+	}, [])
+	console.log('pinStd:', pinScholarships)
 
 	return (
 		<Grid container marginTop={2} marginBottom={4} gap="20px 30px" justifyContent="center">
@@ -28,68 +33,81 @@ function Scholarship(props) {
 					There is no matching scholarship
 				</Typography>
 			) : null}
-			{props.items.map((scholar) => {
-				// check if scholarship is in pin scholarship
-				const active = false 
-				return (
-					<Paper
-						key={scholar._id}
-						sx={{
-							display: 'flex',
-							width: 300,
-							height: 180,
-							flexDirection: 'column',
-							cursor: 'pointer',
-						}}
-						onClick={() => {
-							if (auth && auth.role === 'provider') {
-								router.push({
-									pathname: 'scholarship/DetailScholarship',
-									query: { data: JSON.stringify(scholar) },
-								  });
-								//router.push(`/scholarship/update-scholarship/${scholar._id}`)
-							} else {
-								router.push({
-									pathname: 'scholarship/DetailScholarship',
-									query: { data: JSON.stringify(scholar) },
-								  });
-							}
-						}}
-					>
-						<Grid container direction="row" justifyContent="space-between">
-							<Typography margin={2} marginLeft={2}>
-								{scholar.scholarshipName}
-							</Typography>
-							<Button variant="text" sx={{ display: 'flex', width: 50, height: 50 }}>
-								<PinScholar pin = {active} id = {scholar._id} std = {auth.username}/>
-							</Button>
-						</Grid>
-						<Divider orientation="horizontal" variant="middle" style={{ borderBottomWidth: 2 }} />
-						<Grid margin={1}>
-							{/*//TODO Fix tag pls */}
-							{scholar.tag?.map((tag, idx) => {
-								return (
-									<div key={scholar.scholarshipName + scholar.tag + idx}>
-										<Box
-											sx={{
-												display: 'flex',
-												width: 60,
-												height: 25,
-												backgroundColor: '#e6edec',
-												borderRadius: 1,
-												margin: 1,
-												marginLeft: 2,
-											}}
-										>
-											<Typography marginLeft={1}>{tag}</Typography>
-										</Box>
-									</div>
-								)
-							})}
-						</Grid>
-					</Paper>
-				)
-			})}
+			{props.items
+				.sort((scholarA, scholarB) => {
+					if (pinScholarships.includes(scholarA._id) && pinScholarships.includes(scholarB._id)) {
+						return 0 // keep original order
+					}
+					if (pinScholarships.includes(scholarA._id)) {
+						return -1 // sort a before b
+					}
+					if (pinScholarships.includes(scholarB._id)) {
+						return 1 // sort b before a
+					}
+					return 0
+				})
+				.map((scholar) => {
+					// check if scholarship is in pin scholarship
+					const active = false
+					return (
+						<Paper
+							key={scholar._id}
+							sx={{
+								display: 'flex',
+								width: 300,
+								height: 180,
+								flexDirection: 'column',
+								cursor: 'pointer',
+							}}
+							onClick={() => {
+								if (auth && auth.role === 'provider') {
+									router.push({
+										pathname: 'scholarship/DetailScholarship',
+										query: { data: JSON.stringify(scholar) },
+									})
+									//router.push(`/scholarship/update-scholarship/${scholar._id}`)
+								} else {
+									router.push({
+										pathname: 'scholarship/DetailScholarship',
+										query: { data: JSON.stringify(scholar) },
+									})
+								}
+							}}
+						>
+							<Grid container direction="row" justifyContent="space-between">
+								<Typography margin={2} marginLeft={2}>
+									{scholar.scholarshipName}
+								</Typography>
+								<Button variant="text" sx={{ display: 'flex', width: 50, height: 50 }}>
+									<PinScholar pin={active} id={scholar._id} std={auth.username} />
+								</Button>
+							</Grid>
+							<Divider orientation="horizontal" variant="middle" style={{ borderBottomWidth: 2 }} />
+							<Grid margin={1}>
+								{/*//TODO Fix tag pls */}
+								{scholar.tag?.map((tag, idx) => {
+									return (
+										<div key={scholar.scholarshipName + scholar.tag + idx}>
+											<Box
+												sx={{
+													display: 'flex',
+													width: 60,
+													height: 25,
+													backgroundColor: '#e6edec',
+													borderRadius: 1,
+													margin: 1,
+													marginLeft: 2,
+												}}
+											>
+												<Typography marginLeft={1}>{tag}</Typography>
+											</Box>
+										</div>
+									)
+								})}
+							</Grid>
+						</Paper>
+					)
+				})}
 		</Grid>
 	)
 }

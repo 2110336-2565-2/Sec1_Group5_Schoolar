03b610aba880a38ad99bce8c3c8db701831200d4
NonRoute <chanon.ratn@gmail.com>
2023-04-13 22:12:23 +0700

    Add scholarship icon

--- client/components/Home-page/ScholarshipTag.js
+++ client/components/Home-page/ScholarshipTag.js
@@ -1,3 +1,36 @@
+import AccountBalanceIcon from '@mui/icons-material/AccountBalance'
+import AirlineSeatFlatAngledIcon from '@mui/icons-material/AirlineSeatFlatAngled'
+import ArchitectureIcon from '@mui/icons-material/Architecture'
+import AutorenewIcon from '@mui/icons-material/Autorenew'
+import BiotechIcon from '@mui/icons-material/Biotech'
+import BrushIcon from '@mui/icons-material/Brush'
+import CalculateIcon from '@mui/icons-material/Calculate'
+import ChatIcon from '@mui/icons-material/Chat'
+import CircleIcon from '@mui/icons-material/Circle'
+import ColorLensIcon from '@mui/icons-material/ColorLens'
+import CorporateFareIcon from '@mui/icons-material/CorporateFare'
+import Diversity2Icon from '@mui/icons-material/Diversity2'
+import EngineeringIcon from '@mui/icons-material/Engineering'
+import GavelIcon from '@mui/icons-material/Gavel'
+import HomeIcon from '@mui/icons-material/Home'
+import LocalHospitalIcon from '@mui/icons-material/LocalHospital'
+import MedicalInformationIcon from '@mui/icons-material/MedicalInformation'
+import MedicationLiquidIcon from '@mui/icons-material/MedicationLiquid'
+import MenuBookIcon from '@mui/icons-material/MenuBook'
+import MuseumIcon from '@mui/icons-material/Museum'
+import PaidIcon from '@mui/icons-material/Paid'
+import PetsIcon from '@mui/icons-material/Pets'
+import PsychologyIcon from '@mui/icons-material/Psychology'
+import PublicIcon from '@mui/icons-material/Public'
+import SchoolIcon from '@mui/icons-material/School'
+import ScienceIcon from '@mui/icons-material/Science'
+import SportsTennisIcon from '@mui/icons-material/SportsTennis'
+import StarIcon from '@mui/icons-material/Star'
+import TagIcon from '@mui/icons-material/Tag'
+import TheaterComedyIcon from '@mui/icons-material/TheaterComedy'
+import TranslateIcon from '@mui/icons-material/Translate'
+import TripOriginIcon from '@mui/icons-material/TripOrigin'
+import VaccinesIcon from '@mui/icons-material/Vaccines'
 import { Grid, Typography } from '@mui/material'
 import Chip from '@mui/material/Chip'
 
@@ -36,18 +69,57 @@ const tagColors = {
 	Doctoral: '#00051a',
 }
 
-const Tag = ({ text, color }) => (
+const tagIcons = {
+	'Sci-Math': <ScienceIcon />,
+	'Art-Cal': <CalculateIcon />,
+	'Art-Language': <TranslateIcon />,
+	'Art-Society': <Diversity2Icon />,
+	'Art-General': <ColorLensIcon />,
+	'Faculty of Allied Health Sciences': <MedicationLiquidIcon />,
+	'Faculty of Architecture': <ArchitectureIcon />,
+	'Faculty of Arts': <ChatIcon />,
+	'Faculty of Commerce and Accountancy': <AccountBalanceIcon />,
+	'Faculty of Communication Arts': <TheaterComedyIcon />,
+	'Faculty of Dentistry': <AirlineSeatFlatAngledIcon />,
+	'Faculty of Economics': <PaidIcon />,
+	'Faculty of Education': <MenuBookIcon />,
+	'Faculty of Engineering': <EngineeringIcon />,
+	'Faculty of Fine and Applied Arts': <BrushIcon />,
+	'Faculty of Law': <GavelIcon />,
+	'Faculty of Medicine': <MedicalInformationIcon />,
+	'Faculty of Nursing': <LocalHospitalIcon />,
+	'Faculty of Pharmaceutical Sciences': <VaccinesIcon />,
+	'Faculty of Political Science': <PublicIcon />,
+	'Faculty of Psychology': <PsychologyIcon />,
+	'Faculty of Science': <BiotechIcon />,
+	'Faculty of Sports Science': <SportsTennisIcon />,
+	'Faculty of Veterinary Science': <PetsIcon />,
+	'Full Scholarship': <CircleIcon />,
+	'Partial Scholarship': <TripOriginIcon />,
+	'Renewable Scholarship': <AutorenewIcon />,
+	Fellowship: <CorporateFareIcon />,
+	'High School': <HomeIcon />,
+	Bachelor: <SchoolIcon />,
+	Master: <MuseumIcon />,
+	Doctoral: <StarIcon />,
+}
+
+const Tag = ({ text, color, icon }) => (
 	<Typography
 		sx={{
-			paddingX: 3,
-			paddingY: 0.5,
+			pl: 1.25,
+			pr: 2,
+			py: 0.5,
 			display: 'flex',
 			color: 'white',
 			backgroundColor: color,
 			borderRadius: 5,
 			margin: 0.5,
+			display: 'flex',
+			gap: 0.5,
 		}}
 	>
+		{icon}
 		{text}
 	</Typography>
 )
@@ -58,6 +130,7 @@ const ScholarshipTags = ({ scholar }) => (
 			word-wrap="break-word"
 			text={scholar.typeOfScholarship}
 			color={tagColors[scholar.typeOfScholarship] || 'text.primary'}
+			icon={tagIcons[scholar.typeOfScholarship] || <TagIcon />}
 		/>
 		<Tag
 			word-wrap="break-word"
@@ -68,6 +141,7 @@ const ScholarshipTags = ({ scholar }) => (
 			}}
 			text={scholar.degree}
 			color={tagColors[scholar.degree] || 'text.primary'}
+			icon={tagIcons[scholar.degree] || <TagIcon />}
 		/>
 		<Tag
 			word-wrap="break-word"
@@ -78,6 +152,7 @@ const ScholarshipTags = ({ scholar }) => (
 			}}
 			text={scholar.program}
 			color={tagColors[scholar.program] || 'text.primary'}
+			icon={tagIcons[scholar.program] || <TagIcon />}
 		/>
 	</Grid>
 )
